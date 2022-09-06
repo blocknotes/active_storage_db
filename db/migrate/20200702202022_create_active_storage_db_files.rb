0b5bb4192f27a6cd58d2f281d97b79e5b06ec99c
blocknotes <mat@blocknot.es>
2022-09-06 10:20:17 +0200

    Minor improvements to the create active storage db files migration

--- db/migrate/20200702202022_create_active_storage_db_files.rb
+++ db/migrate/20200702202022_create_active_storage_db_files.rb
@@ -2,12 +2,25 @@
 
 class CreateActiveStorageDBFiles < ActiveRecord::Migration[6.0]
   def change
-    create_table :active_storage_db_files do |t|
+    create_table :active_storage_db_files, id: primary_key_type do |t|
       t.string :ref, null: false
       t.binary :data, null: false
-      t.datetime :created_at, null: false
+
+      if connection.supports_datetime_with_precision?
+        t.datetime :created_at, precision: 6, null: false
+      else
+        t.datetime :created_at, null: false
+      end
 
       t.index [:ref], unique: true
     end
+  end
+
+  private
+
+  def primary_key_type
+    config = Rails.configuration.generators
+    primary_key_type = config.options[config.orm][:primary_key_type]
+    primary_key_type || :primary_key
   end
 end
