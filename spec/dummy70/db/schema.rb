cfb5b688c0f712028182cc930dee7b18fffce687
Mattia Roccoberton <mattia.roccoberton@telepass.com>
2023-11-01 22:34:41 +0100

    feat: replace failing functions for MSSQL

--- spec/dummy70/db/schema.rb
+++ spec/dummy70/db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_02_010101) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
