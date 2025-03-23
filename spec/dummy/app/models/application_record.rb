6e469dd0c9aecdb4c3c32600c4f3093fcf404c65
Mattia Roccoberton <mat@blocknot.es>
2025-03-23 09:07:34 +0100

    test: Update dummy app

---
+++ spec/dummy/app/models/application_record.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ApplicationRecord < ActiveRecord::Base
+  if Gem::Version.new(Rails.version) >= Gem::Version.new('7.0')
+    primary_abstract_class
+  else
+    self.abstract_class = true
+  end
+end
